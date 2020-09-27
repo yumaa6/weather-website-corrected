6e9fdab6d937cc39afd0ede08529523587f57c27
yumaa6 <aljoschagamer@gmail.com>
2020-09-27 18:32:03 +0200

    Feels like added and long. and lat. switched in forecast.js

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,17 +1,18 @@
 const request = require('request')
 
 const forecast = (latitude, longtitude, callback) => {
-    const url = `http://api.weatherstack.com/current?access_key=a51e51a3ffc83ccdb88c6db940bb219b&query=${latitude},${longtitude}&units=m`
+    const url = `http://api.weatherstack.com/current?access_key=a51e51a3ffc83ccdb88c6db940bb219b&query=${longtitude},${latitude}&units=m`
     
     request ({url, json:true}, (error, {body})=> {
         if (error){
-            callback('Unable to connect to service')
+            callback('Unable to connect to service', undefined)
       } else if (body.error) {
-            callback('Invalid Latitude & Longtitude')
+            callback('Invalid Latitude & Longtitude', undefined)
             console.log(url)
 
       } else {
-          callback(null, `It is currently ${body.current.temperature}°C out` )
+          callback(null, `It is currently ${body.current.temperature}°C out.
+                          It feels like ${body.current.feelslike}°C out.` )
 
       }
     })
